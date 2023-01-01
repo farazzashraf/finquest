a9c9eabc3af632046e5d9b6a97e98aa7826380be
Faraz <farazashraf413@gmail.com>
2023-01-01 20:31:34 +0530

    commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// const hamburgerButton = document.querySelector('.aside');
+// const menu = document.querySelector('.sidebar');
+
+// function openHamburgerBtn() {
+//     menu.style.display = 'block';
+// }
+
+// function closeHamburgerBtn(event) {
+// }
+
+// hamburgerButton.addEventListener('click', openHamburgerBtn);
+// document.addEventListener('click', openHamburgerBtn);
